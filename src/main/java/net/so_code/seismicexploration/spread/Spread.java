b5d2852801c534ecf8b840aa0e581e47ff3a9c25
Olivier Sechet <osechet@gmail.com>
2025-05-11 10:26:16 +0200

    chore: remove unused method

--- src/main/java/net/so_code/seismicexploration/spread/Spread.java
+++ src/main/java/net/so_code/seismicexploration/spread/Spread.java
@@ -3,22 +3,15 @@ package net.so_code.seismicexploration.spread;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
-import java.util.Map;
 import java.util.Set;
-import java.util.stream.Collectors;
-import javax.annotation.Nonnull;
 import org.slf4j.Logger;
 import com.mojang.logging.LogUtils;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction.Axis;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.datafix.DataFixTypes;
-import net.minecraft.world.level.Level;
-import net.minecraft.world.level.material.MapColor.Brightness;
 import net.minecraft.world.level.saveddata.SavedData;
 import net.minecraft.world.level.saveddata.SavedDataType;
 import net.minecraft.world.level.storage.DimensionDataStorage;
-import net.so_code.seismicexploration.blockentity.SensorBlockEntity;
 
 public class Spread extends SavedData {
 
@@ -52,20 +45,5 @@ public class Spread extends SavedData {
 
     public Set<BlockPos> getPlacedSensors() {
         return Collections.unmodifiableSet(placedSensors);
-    }
-
-    @Nonnull
-    public Object getSlice(final Level level, final int x, final int z, final Axis axis) {
-        // TODO: we should use getPackedId instead of calculateARGBColor
-        final Map<BlockPos, Integer> blocks = placedSensors.stream() //
-                .map(pos -> (SensorBlockEntity) level.getBlockEntity(pos)) //
-                .map(sensor -> sensor.getBlocks()) //
-                .flatMap(map -> map.entrySet().stream()) //
-                .collect(Collectors.toMap(Map.Entry::getKey, //
-                        entry -> entry.getValue().calculateARGBColor(Brightness.NORMAL), //
-                        (existing, replacement) -> existing // keep the first value found
-                ));
-
-        return null;
     }
 }
